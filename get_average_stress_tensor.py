2d4c03403aaa9637bccb7beba55ad80d98aafe94
Mahdi Davari <mahdi.davari@stonybrook.edu>
2018-07-05 12:45:15 -0400

    get_file update for ensemble average

--- get_average_stress_tensor.py
+++ get_average_stress_tensor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3.3
 
 import os
 import sys
@@ -24,12 +24,14 @@ if (int(num_discard) > tot or int(num_discard) < 0):
 if num_discard == '':
     num_discard = 1000
 
-equilibrated = tot - num_discard
+equilibrated = tot - int(num_discard)
 
-os.system("grep 'in kB' OUTCAR | tail -"+str(equilibrated)+"  > tmp_sig")
+os.system("grep -A 1 'in kB' OUTCAR | tail -"+str(3*equilibrated)+"  > tmp_sig")
 
 for i in range(6):
     os.system("grep 'in kB' tmp_sig | awk 'BEGIN {sig=0.} {sig+=$"+str(i+3)+"} END {print sig/"+str(equilibrated)+"}' >> tmp_file")
+os.system("grep 'Pullay' tmp_sig | awk 'BEGIN {sig=0.} {sig+=$4} END {print sig/"+str(equilibrated)+"}' >> tmp_file")
+os.system("grep 'Pullay' tmp_sig | awk 'BEGIN {sig=0.} {sig+=$9} END {print sig/"+str(equilibrated)+"}' >> tmp_file")
 os.system('rm -f tmp_sig')
 
 
@@ -41,13 +43,21 @@ with open('tmp_file') as f:
     sig4 = float(f.readline())
     sig5 = float(f.readline())
     sig6 = float(f.readline())
+    ext_p= float(f.readline())
+    pullay_s= float(f.readline())
 
-    print('SigXX', '%10.3f' % sig1)
-    print('SigYY', '%10.3f' % sig2)
-    print('SigZZ', '%10.3f' % sig3)
-    print('SigXY', '%10.3f' % sig4)
-    print('SigYZ', '%10.3f' % sig5)
-    print('SigZX', '%10.3f' % sig6)
+
+#    print('SigXX %10.3f' % sig1)
+#    print('SigYY %10.3f' % sig2)
+#    print('SigZZ %10.3f' % sig3)
+#    print('SigXY %10.3f' % sig4)
+#    print('SigYZ %10.3f' % sig5)
+#    print('SigZX %10.3f' % sig6)
+#    print('external pressure %10.3f' %ext_p)
+#    print('Pullay Stress %10.3f' %pullay_s)
+    print(' in kB    ', str(sig1), str(sig2), str(sig3), str(sig4), str(sig5), str(sig6), sep="    ")
+    print('  external pressure =      ', ext_p,' kB  Pullay stress =     ', pullay_s, ' kB')
+    print('  ')
     f.close()
 
 os.system('rm -f tmp_file')
