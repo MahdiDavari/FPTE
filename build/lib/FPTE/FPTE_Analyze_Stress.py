ensemble average issue is fixed for Total+kin

build/lib/FPTE/FPTE_Analyze_Stress.py
@@ -225,11 +225,11 @@ def fpte_analyze():
         flstres = open(Dstn+'_Lagrangian-stress.dat', 'w')
         fpstres = open(Dstn+  '_Physical-stress.dat', 'w')
 
-        flstres.write(' Lagrangian strain and Lagrangian stresses (LS) in Voigt notation for '+ Dstn +'.')
-        l=' Lag. strain          LS1              LS2              LS3              LS4              LS5              LS6 '
+        flstres.write(' Lagrangian strain and Lagrangian stresses (LS) in Voigt notation for '+ Dstn +'. \n')
+        l=' Lag. strain          LS1              LS2              LS3              LS4              LS5              LS6 \n'
         flstres.write(l) 
-        fpstres.write(' Lagrangian strain and physical stresses (PS) in Voigt notation for '+ Dstn +'.')
-        fpstres.write(' Lag. strain         PS1          PS2          PS3          PS4          PS5          PS6 ')
+        fpstres.write(' Lagrangian strain and physical stresses (PS) in Voigt notation for '+ Dstn +'. \n')
+        fpstres.write(' Lag. strain         PS1          PS2          PS3          PS4          PS5          PS6  \n')
 
         for j in range(1, NoP+1):
             if (j<10):
@@ -310,6 +310,8 @@ def fpte_analyze():
                                 l1 = fstres.readline()
                                 l2 = fstres.readline()
                             pullay_stree = l2.split()[-2]
+                            print(pullay_stree)
+                            print(l2)
                             print('The external pressure is '+str(float(pullay_stree)/10.))
                             sig[0,0] = str(float(l1.split()[-6])-float(pullay_stree))
                             sig[0,1] = l1.split()[-3]
@@ -345,14 +347,14 @@ def fpte_analyze():
                                         +'   '+'%10.3f'%sig[2,2]\
                                         +'   '+'%10.3f'%sig[1,2]\
                                         +'   '+'%10.3f'%sig[0,2]\
-                                        +'   '+'%10.3f'%sig[0,1])
+                                        +'   '+'%10.3f'%sig[0,1]+'\n')
 
                 flstres.write(strain +'   '+'%14.8f'%tao[0,0]\
                                         +'   '+'%14.8f'%tao[1,1]\
                                         +'   '+'%14.8f'%tao[2,2]\
                                         +'   '+'%14.8f'%tao[1,2]\
                                         +'   '+'%14.8f'%tao[0,2]\
-                                        +'   '+'%14.8f'%tao[0,1])
+                                        +'   '+'%14.8f'%tao[0,1]+'\n')
                 os.chdir('../')
         flstres.close()
         fpstres.close()
@@ -466,7 +468,7 @@ def fpte_analyze():
                     if (ordr == 3):
                         Cij = coeffs[j-2] * CONV * 0.001 # in TPa unit
 
-                    fD.write('%13.10f'%emax, '%18.6f'%Cij)
+                    fD.write('%13.10f'%emax + ' ' + '%18.6f'%Cij)
                     if (abs(strain[0]+emax) < 1.e-7):
                         strain.pop(0)
                         stress.pop(0)
@@ -495,7 +497,7 @@ def fpte_analyze():
                         S = S + (Yfit-Y)**2
 
                     CV = np.sqrt(S/len(strain))
-                    fE.write( '%13.10f'%emax, CV)
+                    fE.write( '%13.10f'%emax+ ' '+ '%13.10f'%CV)
 
                     if (abs(strain[0]+emax) < 1.e-7):
                         strain.pop(0)

build/lib/FPTE/FPTE_Result_Stress_2nd.py
@@ -22,7 +22,7 @@
 #__________________________________________________________________________________________________
 
 from sys   import stdin
-from numpy import *
+# from numpy import *
 import numpy as np
 import subprocess
 import os.path
@@ -41,41 +41,41 @@ def fpte_results():
                 C11     C12     C12      0       0       0                  \n\
                 C12     C11     C12      0       0       0                  \n\
                 C12     C12     C11      0       0       0                  \n\
-                    0       0       0      C44      0       0                  \n\
-                    0       0       0       0      C44      0                  \n\
-                    0       0       0       0       0      C44                 \n',\
+                 0       0       0      C44      0       0                  \n\
+                 0       0       0       0      C44      0                  \n\
+                 0       0       0       0       0      C44                 \n',\
     'CII':'\
         for, space group-number between 195 and 206, Cubic II structure.       \n\n\
                 C11     C12     C12      0       0       0                  \n\
                 C12     C11     C12      0       0       0                  \n\
                 C12     C12     C11      0       0       0                  \n\
-                    0       0       0      C44      0       0                  \n\
-                    0       0       0       0      C44      0                  \n\
-                    0       0       0       0       0      C44                 \n',\
+                 0       0       0      C44      0       0                  \n\
+                 0       0       0       0      C44      0                  \n\
+                 0       0       0       0       0      C44                 \n',\
     'HI':'\
         for, space group-number between 177 and 194, Hexagonal I structure.    \n\n\
                 C11     C12     C13      0       0       0                  \n\
                 C12     C11     C13      0       0       0                  \n\
                 C13     C13     C33      0       0       0                  \n\
-                    0       0       0      C44      0       0                  \n\
-                    0       0       0       0      C44      0                  \n\
-                    0       0       0       0       0   (C11-C12)/2            \n',\
+                 0       0       0      C44      0       0                  \n\
+                 0       0       0       0      C44      0                  \n\
+                 0       0       0       0       0   (C11-C12)/2            \n',\
     'HII':'\
         for, space group-number between 168 and 176, Hexagonal II structure.   \n\n\
                 C11     C12     C13      0       0       0                  \n\
                 C12     C11     C13      0       0       0                  \n\
                 C13     C13     C33      0       0       0                  \n\
-                    0       0       0      C44      0       0                  \n\
-                    0       0       0       0      C44      0                  \n\
-                    0       0       0       0       0   (C11-C12)/2            \n',\
+                 0       0       0      C44      0       0                  \n\
+                 0       0       0       0      C44      0                  \n\
+                 0       0       0       0       0   (C11-C12)/2            \n',\
     'RI':'\
         for, space group-number between 149 and 167, Rhombohedral I structure. \n\n\
                 C11     C12     C13     C14      0       0                  \n\
                 C12     C11     C13    -C14      0       0                  \n\
                 C13     C13     C33      0       0       0                  \n\
                 C14    -C14      0      C44      0       0                  \n\
-                    0       0       0       0      C44     C14                 \n\
-                    0       0       0       0      C14  (C11-C12)/2            \n',\
+                 0       0       0       0      C44     C14                 \n\
+                 0       0       0       0      C14  (C11-C12)/2            \n',\
     'RII':'\
         for, space group-number between 143 and 148, Rhombohedral II structure.\n\n\
                 C11     C12     C13     C14     C15      0                  \n\
@@ -83,38 +83,38 @@ def fpte_results():
                 C13     C13     C33      0       0       0                  \n\
                 C14    -C14      0      C44      0     -C15                 \n\
                 C15    -C15      0       0      C44     C14                 \n\
-                    0       0       0     -C15     C14  (C11-C12)/2            \n',\
+                 0       0       0     -C15     C14  (C11-C12)/2            \n',\
     'TI':'\
         for, space group-number between 89 and 142, Tetragonal I structure.    \n\n\
                 C11     C12     C13      0       0       0                  \n\
                 C12     C11     C13      0       0       0                  \n\
                 C13     C13     C33      0       0       0                  \n\
-                    0       0       0      C44      0       0                  \n\
-                    0       0       0       0      C44      0                  \n\
-                    0       0       0       0       0      C66                 \n',\
+                 0       0       0      C44      0       0                  \n\
+                 0       0       0       0      C44      0                  \n\
+                 0       0       0       0       0      C66                 \n',\
     'TII':'\
         for, space group-number between 75 and 88, Tetragonal II structure.    \n\n\
                 C11     C12     C13      0       0      C16                 \n\
                 C12     C11     C13      0       0     -C16                 \n\
                 C13     C13     C33      0       0       0                  \n\
-                    0       0       0      C44      0       0                  \n\
-                    0       0       0       0      C44      0                  \n\
+                 0       0       0      C44      0       0                  \n\
+                 0       0       0       0      C44      0                  \n\
                 C16    -C16      0       0       0      C66                 \n',\
     'O':'\
         for, space group-number between 16 and 74, Orthorhombic structure.     \n\n\
                 C11     C12     C13      0       0       0                  \n\
                 C12     C22     C23      0       0       0                  \n\
                 C13     C23     C33      0       0       0                  \n\
-                    0       0       0      C44      0       0                  \n\
-                    0       0       0       0      C55      0                  \n\
-                    0       0       0       0       0      C66                 \n',\
+                 0       0       0      C44      0       0                  \n\
+                 0       0       0       0      C55      0                  \n\
+                 0       0       0       0       0      C66                 \n',\
     'M':'\
         for, space group-number between 3 and 15, Monoclinic structure.        \n\n\
                 C11     C12     C13      0       0      C16                 \n\
                 C12     C22     C23      0       0      C26                 \n\
                 C13     C23     C33      0       0      C36                 \n\
-                    0       0       0      C44     C45      0                  \n\
-                    0       0       0      C45     C55      0                  \n\
+                 0       0       0      C44     C45      0                  \n\
+                 0       0       0      C45     C55      0                  \n\
                 C16     C26     C36      0       0      C66                 \n',\
     'N':'\
         for, space group-number between 1 and 2, Triclinic structure.          \n\n\
@@ -222,7 +222,7 @@ def fpte_results():
     #%%%--- Making the Matrix  ---%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%
     if (LC == 'CI' or \
         LC == 'CII'):
-        Matrix = mat([[1.0,  5.0,  0.0],
+        Matrix = np.mat([[1.0,  5.0,  0.0],
                     [2.0,  4.0,  0.0],
                     [3.0,  3.0,  0.0],
                     [0.0,  0.0,  4.0],
@@ -231,7 +231,7 @@ def fpte_results():
 
     if (LC == 'HI' or \
         LC == 'HII'):
-        Matrix = mat([[ 1, 2, 3, 0, 0],
+        Matrix = np.mat([[ 1, 2, 3, 0, 0],
                     [ 2, 1, 3, 0, 0],
                     [ 0, 0, 3, 3, 0],
                     [ 0, 0, 0, 0, 4],
@@ -245,7 +245,7 @@ def fpte_results():
                     [-2, 2, 0, 0, 0]])
 
     if (LC == 'RI'):
-        Matrix = mat([[ 1, 2, 3, 4, 0, 0],
+        Matrix = np.mat([[ 1, 2, 3, 4, 0, 0],
                     [ 2, 1, 3,-4, 0, 0],
                     [ 0, 0, 3, 0, 3, 0],
                     [ 0, 0, 0,-1, 0, 4],
@@ -259,7 +259,7 @@ def fpte_results():
                     [-2, 2, 0, 2, 0, 0]])
 
     if (LC == 'RII'):
-        Matrix = mat([[ 1, 2, 3, 4, 5, 0, 0],
+        Matrix = np.mat([[ 1, 2, 3, 4, 5, 0, 0],
                     [ 2, 1, 3,-4,-5, 0, 0],
                     [ 0, 0, 3, 0, 0, 3, 0],
                     [ 0, 0, 0,-1,-6, 0, 4],
@@ -273,7 +273,7 @@ def fpte_results():
                     [-2, 2, 0, 2,-6, 0, 0]])
 
     if (LC == 'TI'):
-        Matrix = mat([[ 1, 2, 3, 0, 0, 0],
+        Matrix = np.mat([[ 1, 2, 3, 0, 0, 0],
                     [ 2, 1, 3, 0, 0, 0],
                     [ 0, 0, 3, 3, 0, 0],
                     [ 0, 0, 0, 0, 4, 0],
@@ -287,7 +287,7 @@ def fpte_results():
                     [ 0, 0, 0, 0, 0,-4]])
 
     if (LC == 'TII'):
-        Matrix = mat([[ 1, 2, 3, 6, 0, 0, 0],
+        Matrix = np.mat([[ 1, 2, 3, 6, 0, 0, 0],
                     [ 2, 1, 3,-6, 0, 0, 0],
                     [ 0, 0, 3, 0, 3, 0, 0],
                     [ 0, 0, 0, 0, 0, 4, 0],
@@ -301,7 +301,7 @@ def fpte_results():
                     [ 0, 0, 0, 8, 0, 0,-4]])
 
     if (LC == 'O'):
-        Matrix = mat([[1, 2, 3, 0, 0, 0, 0, 0, 0],
+        Matrix = np.mat([[1, 2, 3, 0, 0, 0, 0, 0, 0],
                     [0, 1, 0, 2, 3, 0, 0, 0, 0],
                     [0, 0, 1, 0, 2, 3, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 4, 0, 0],
@@ -321,7 +321,7 @@ def fpte_results():
                     [0, 0, 0, 0, 0, 0, 0, 0,-3]])
 
     if (LC == 'M'):
-        Matrix = mat([[ 1, 2, 3, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        Matrix = np.mat([[ 1, 2, 3, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [ 0, 1, 0, 0, 2, 3, 6, 0, 0, 0, 0, 0, 0],
                     [ 0, 0, 1, 0, 0, 2, 0, 3, 6, 0, 0, 0, 0],
                     [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 0, 0],
@@ -353,7 +353,7 @@ def fpte_results():
                     [ 0, 0, 0, 5, 0, 0, 4, 0, 6, 0, 0,-3, 0]])
 
     if (LC == 'N'):
-        Matrix = mat([[ 1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        Matrix = np.mat([[ 1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [ 0, 1, 0, 0, 0, 0, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [ 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0],
                     [ 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 4, 5, 6, 0, 0, 0],
@@ -530,8 +530,8 @@ def fpte_results():
         sys.exit('\n.... Oops ERROR: Something is WRONG in the "FPTE_2nd.in" file !?!?!?\n')
 
     sigma = np.array(sigma)
-    ci    = linalg.lstsq(Matrix,sigma)
-    C     = zeros((6,6))
+    ci    = np.linalg.lstsq(Matrix,sigma)
+    C     = np.zeros((6,6))
 
     #-- Cubic structures ------------------------------------------------------------------------------
     if (LC == 'CI' or \
@@ -691,7 +691,7 @@ def fpte_results():
     GV = ((C[0,0]+C[1,1]+C[2,2])-(C[0,1]+C[0,2]+C[1,2])+3*(C[3,3]+C[4,4]+C[5,5]))/15
     EV = (9*BV*GV)/(3*BV+GV)
     nuV= (1.5*BV-GV)/(3*BV+GV)
-    S  = linalg.inv(C)
+    S  = np.linalg.inv(C)
     BR = 1/(S[0,0]+S[1,1]+S[2,2]+2*(S[0,1]+S[0,2]+S[1,2]))
     GR =15/(4*(S[0,0]+S[1,1]+S[2,2])-4*(S[0,1]+S[0,2]+S[1,2])+3*(S[3,3]+S[4,4]+S[5,5]))
     ER = (9*BR*GR)/(3*BR+GR)
@@ -705,63 +705,61 @@ def fpte_results():
 
     #%%%--- Writing the output file ---%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!
     fo = open('FPTE_2nd.out', 'w')
-    print >>fo,  '    The output of FPTE code                                              \n'\
+    fo.write('    The output of FPTE code                                              \n'\
                 '    Today is '+ time.asctime() +                                           '\n'\
                                                                                             '\n'\
                 '    Symmetry of the second-order elastic constant matrix in Voigt notation. \n'\
                     + head[LC] +                                                             '\n'\
-                '    Elastic constant (stiffness) matrix in GPa:                             \n'
+                '    Elastic constant (stiffness) matrix in GPa:                             \n')
 
     for i in range(0,6):
-        print >>fo, '',
         for j in range(0,6):
-            print >>fo, '%11.1f'%(C[i,j]),
-        print >>fo
+            fo.write('%11.1f'%(C[i,j]))
+        fo.write(' \n')
 
-    print >>fo,'\n\n    Elastic compliance matrix in 1/GPa: \n'
+    fo.write('\n\n    Elastic compliance matrix in 1/GPa: \n')
 
     for i in range(0,6):
-        print >>fo, '',
         for j in range(0,6):
-            print >>fo, '%11.5f'%(S[i,j]),
-        print >>fo
+            fo.write('%11.5f'%(S[i,j]))
+        fo.write('\n')
 
-    print >>fo, '\n'+ lineseparator +'\n'
+    fo.write('\n'+ lineseparator +'\n')
 
-    print >>fo, '    Voigt bulk  modulus, B_V = {0}  GPa'.format('%8.2f'%(BV))
-    print >>fo, '    Voigt shear modulus, G_V = {0}  GPa'.format('%8.2f'%(GV)) + '\n'
+    fo.write('    Voigt bulk  modulus, B_V = {0}  GPa'.format('%8.2f'%(BV)))
+    fo.write('    Voigt shear modulus, G_V = {0}  GPa'.format('%8.2f'%(GV)) + '\n')
 
-    print >>fo, '    Reuss bulk  modulus, B_R = {0}  GPa'.format('%8.2f'%(BR))
-    print >>fo, '    Reuss shear modulus, G_R = {0}  GPa'.format('%8.2f'%(GR)) + '\n'
+    fo.write('    Reuss bulk  modulus, B_R = {0}  GPa'.format('%8.2f'%(BR)))
+    fo.write('    Reuss shear modulus, G_R = {0}  GPa'.format('%8.2f'%(GR)) + '\n')
 
-    print >>fo, '    Hill bulk  modulus,  B_H = {0}  GPa'.format('%8.2f'%(BH))
-    print >>fo, '    Hill shear modulus,  G_H = {0}  GPa'.format('%8.2f'%(GH))
+    fo.write('    Hill bulk  modulus,  B_H = {0}  GPa'.format('%8.2f'%(BH)))
+    fo.write('    Hill shear modulus,  G_H = {0}  GPa'.format('%8.2f'%(GH)))
 
-    print >>fo, '\n'+ lineseparator +'\n'
+    fo.write('\n'+ lineseparator +'\n')
 
-    print >>fo, '    Voigt Young modulus,  E_V = {0}  GPa'.format('%8.2f'%(EV))
-    print >>fo, '    Voigt Poisson ratio, nu_V = {0}'     .format('%8.2f'%(nuV)) + '\n'
+    fo.write('    Voigt Young modulus,  E_V = {0}  GPa'.format('%8.2f'%(EV)))
+    fo.write('    Voigt Poisson ratio, nu_V = {0}'     .format('%8.2f'%(nuV)) + '\n')
 
-    print >>fo, '    Reuss Young modulus,  E_R = {0}  GPa'.format('%8.2f'%(ER))
-    print >>fo, '    Reuss Poisson ratio, nu_R = {0}'     .format('%8.2f'%(nuR)) + '\n'
+    fo.write('    Reuss Young modulus,  E_R = {0}  GPa'.format('%8.2f'%(ER)))
+    fo.write('    Reuss Poisson ratio, nu_R = {0}'     .format('%8.2f'%(nuR)) + '\n')
 
-    print >>fo, '    Hill Young modulus,   E_H = {0}  GPa'.format('%8.2f'%(EH))
-    print >>fo, '    Hill Poisson ratio,  nu_H = {0}'     .format('%8.2f'%(nuH))
+    fo.write('    Hill Young modulus,   E_H = {0}  GPa'.format('%8.2f'%(EH)))
+    fo.write('    Hill Poisson ratio,  nu_H = {0}'     .format('%8.2f'%(nuH)))
 
-    print >>fo, '\n'+ lineseparator +'\n'
+    fo.write('\n'+ lineseparator +'\n')
 
-    print >>fo, '    Elastic Anisotropy in polycrystalline, AVR = {0} %'.format('%8.3f'%(AVR))
+    fo.write('    Elastic Anisotropy in polycrystalline, AVR = {0} %'.format('%8.3f'%(AVR)))
 
-    print >>fo, '\n'+ lineseparator +'\n'
+    fo.write('\n'+ lineseparator +'\n')
 
-    print >>fo, '    Eigenvalues of elastic constant (stiffness) matrix:   \n'
+    fo.write('    Eigenvalues of elastic constant (stiffness) matrix:   \n')
 
-    eigval=linalg.eig(C)
+    eigval = np.linalg.eig(C)
     for i in range(6):
-        print >>fo,'%16.1f' % float(eigval[0][i])
+        fo.write('%16.1f' % float(eigval[0][i]))
 
-    print >>fo,'\n    ... Have a G00D Day, Week, Month, Year, and Century (if you are lucky) ...    '\
-            '\n               Bye-Bye! Tschuess! Ciao! Poka! Zia Jian! KhodaHafez!             \n'
+    fo.write('\n    ... Have a G00D Day, Week, Month, Year, and Century (if you are lucky) ...    '\
+            '\n               Bye-Bye! Tschuess! Ciao! Poka! Zia Jian! KhodaHafez!             \n')
     fo.close()
 
     #%%%--- Writing Cij in stdout ---%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!%!
@@ -771,11 +769,9 @@ def fpte_results():
                 '    Elastic constant (stiffness) matrix in GPa:                             \n')
 
     for i in range(0,6):
-        print(      '')
-        for j in range(0,6):
-            print(      '%11.1f'%(C[i,j]))  
-        print( )
-
+        print('%13.3f'%C[i][0]+'%13.3f'%C[i][1]+'%13.3f'%C[i][2]+'%13.3f'%C[i][3]+'%13.3f'%C[i][4]+'%13.3f'%C[i][5])
+    print('\n')
+    print('For more information see the FPTE_2nd.out file.')
     #--------------------------------------------------------------------------------------------------
     os.chdir('../')
     os.system('cp -f Stress-vs-Strain/FPTE_2nd.out .')