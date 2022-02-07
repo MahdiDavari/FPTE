cf76ca44dfbbce17ef2f7f81b5dfd6001d8e54c1
Mahdi Davari <8115151+MahdiDavari@users.noreply.github.com>
2022-02-06 21:11:35 -0500

    setup updated with new standards

--- setup.py
+++ setup.py
@@ -1,38 +1,110 @@
-import setuptools
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
+"""setup file for FPTE pkg
+"""
+import sys
+
+from setuptools import setup, find_packages
+
+from .src import FPTE
+from .src.FPTE import _min_dependencies as min_deps  # noqa
+
+DISTNAME = "FPTE",
+VERSION = FPTE.__version__
+SCRIPTS = ["FPTE"]
+AUTHOR = "Mahdi Davari"
+AUTHOR_EMAIL = "Mahdi.Davari@iCloud.com"
+DESCRIPTION = "The FPTE package is a collection of tools for finite pressure temperature " \
+              "elastic constants calculation. Features include, but are not limited to " \
+              "stress-strain method for getting second order elastic tensors using DFT " \
+              "package VASP as well as, ab initio molecular dynamic method for temperature" \
+              "dependent elastic constants."
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+LONG_DESCRIPTION_CONTENT = "text/markdown"
+URL = "https://github.com/MahdiDavari/FPTE"
+LICENSE = ""
+PROJECT_URLS = {
+    "Documentation": "https://github.com/MahdiDavari/FPTE/blob/master/README.md",
+    "Source Code": "https://github.com/MahdiDavari/FPTE",
+    "Bug Reports": "https://github.com/MahdiDavari/FPTE/issues",
+}
+
+DOWNLOAD_URL = "https://pypi.org/project/FPTE/#files"
+
+SETUPTOOLS_COMMANDS = {
+    "develop",
+    "release",
+    "bdist_egg",
+    "bdist_rpm",
+    "bdist_wininst",
+    "install_egg_info",
+    "build_sphinx",
+    "egg_info",
+    "easy_install",
+    "upload",
+    "bdist_wheel",
+    "--single-version-externally-managed",
+}
+if SETUPTOOLS_COMMANDS.intersection(sys.argv):
+    extra_setuptools_args = dict(
+        zip_safe=False,
+        include_package_data=True,
+        extras_require={
+            key: min_deps.tag_to_packages[key]
+            for key in ["examples", "docs", "tests"]
+        },
+    )
+else:
+    extra_setuptools_args = dict()
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def setup_package():
+    """main function for metadata and instalation
+    """
+    metadata = dict(
+        name=DISTNAME,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        scripts=SCRIPTS,
+        download_url=DOWNLOAD_URL,
+        project_urls=PROJECT_URLS,
+        version=VERSION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type=LONG_DESCRIPTION_CONTENT,
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        include_package_data=True,
+        install_requires=min_deps.tag_to_packages["install"],
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "Development Status :: 3 - Stable",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
 
-setuptools.setup(
-     name='FPTE',  
-     version='1.1.0a3',
-     scripts=['FPTE'] ,
-     author="Mahdi Davari",
-     author_email="Mahdi.Davari@icloud.com",
-     description="The FPTE package is a collection of tools for finite pressure temperature elastic constants calculation. Features include, but are not limited to stress-strain method for getting second order elastic tensors using DFT package VASP as well as, ab initio molecular dynamic method for temperature dependent elastic constatns. The package is free and ...",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/MahdiDavari/FPTE",
-     packages=setuptools.find_packages(where='src'),
-     package_dir={'': 'src'},
-     include_package_data=True,
-     install_requires=['matplotlib', 'numpy', 'pandas'],
+        python_requires=">=3.7",
+        package_data={"": ["*.json", "*.yaml"]},
+        **extra_setuptools_args,
+    )
+    setup(**metadata)
 
-     classifiers=[
-         "Development Status :: 3 - Alpha",
-         "Programming Language :: Python :: 2",
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
-    project_urls={
-        'Bug Reports': 'https://github.com/MahdiDavari/FPTE/issues',
-        'Source': 'https://github.com/MahdiDavari/FPTE/src',
-    },
-)
+
+if __name__ == "__main__":
+    setup_package()
