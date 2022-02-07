4da1951d7851515db96d9cb41703e440cd060eaa
Mahdi Davari <8115151+MahdiDavari@users.noreply.github.com>
2022-02-06 21:11:45 -0500

    version bump

--- src/FPTE/__init__.py
+++ src/FPTE/__init__.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 # Distributed under the terms of the MIT License.
 
-"""
-The FPTE package is a collection of tools for finite pressure temperature elastic constants calculation. Features include, but are not limited to stress-strain method for getting second order elastic tensors using DFT package VASP as well as, ab initio molecular dynamic method for temperature dependent elastic constatns. The package is free and ...
+"""The FPTE package is a collection of tools for finite pressure temperature elastic constants calculation.
+ Features include, but are not limited to stress-strain method for getting second order elastic tensors 
+ using DFT package VASP as well as, ab initio molecular dynamic method for temperature dependent elastic 
+ constatns. The package is free and ...
 """
 
 
 __author__ = "Mahdi Davari"
 __email__ = "Mahdi.Davari@iCloud.com"
-__version__ = "1.1.0a2"
+__version__ = "1.2.0"
  
+
